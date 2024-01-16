7e9f6d9600834c463d660960f6d774bfccfabb65
Bara Andrei Mihai <andreibara0@gmail.com>
2024-01-16 20:56:32 +0200

    added: search on google, open first link

--- register-form/src/test/java/tests/GoogleSearchTest.java
+++ register-form/src/test/java/tests/GoogleSearchTest.java
@@ -1,7 +1,10 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -20,5 +23,15 @@ public class GoogleSearchTest {
     {
         driver = new ChromeDriver();
         driver.get("https://google.com");
+
+        WebElement acceptCookiesButton = driver.findElement(By.id("L2AGLb"));
+        acceptCookiesButton.click();
+
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("Google Atelierul Digital", Keys.ENTER);
+
+        WebElement googleAtelierulDigitalLInk = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/span/a/h3"));
+        googleAtelierulDigitalLInk.click();
     }
+
 }
