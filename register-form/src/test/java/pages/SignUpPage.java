2abcf37b8990feb322519d27218e3ce6ca61e03e
Bara Andrei Mihai <andreibara0@gmail.com>
2024-01-17 00:05:48 +0200

    update: submit

--- register-form/src/test/java/pages/SignUpPage.java
+++ register-form/src/test/java/pages/SignUpPage.java
@@ -158,6 +158,7 @@ public class SignUpPage {
         selectDateOfBirth(dateOfBirth);
         selectNationality(nationality);
         checkTermsAndConditions(isTermsAndConditionsChecked);
+        submitButton.submit();
     }
 
     public boolean checkErr(String expectedErr, String errorType) {
